1670d1527441b74edd6d7e082d8016c13ee8c6fb
richiebono <richiebono@gmail.com>
2025-04-17 17:18:16 -0300

    feature - remove dist folder from Git control

--- dist/shared/infrastructure/inversify/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TYPES = void 0;
-exports.TYPES = {
-    IResourceService: Symbol.for('IResourceService'),
-    IUserService: Symbol.for('IUserService'),
-    // Add other types as needed
-};
